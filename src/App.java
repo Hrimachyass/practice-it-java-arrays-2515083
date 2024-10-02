dc6e6cae8c072630d849f9716ed4a0e1498420df
Hrimachyass <101400246+Hrimachyass@users.noreply.github.com>
2024-10-02 09:34:01 +0000

    iteratig through arrays

--- src/App.java
+++ src/App.java
@@ -1,9 +1,23 @@
+import java.util.Arrays;
+
 public class App {
   
   public static void main(String[] args) {
-    double[] lotteryNums = {45, 92, 38, 33, 21};
-    System.out.println(lotteryNums[2]);
-    lotteryNums[2] = 40;
-    System.err.println(lotteryNums[2]);
+    int [] arr = new int[]{45, 92, 38, 33, 21};
+    for(int i=0; i<arr.length; i++){
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    for(int i=arr.length-1; i>0; i--){
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+    // Java Stream: c'est une maniere plus simple de parcourir un tableau.
+    // ca permet de faire aussi des calculs comme la somme, la moyenne etc
+    Arrays.stream(arr).forEach(System.out::println);
+    //Arrays.stream(arr).filter(null);
+    //Arrays.stream(arr).average();
+    //Arrays.stream(arr).sum();
   }
 }
